eb5400f2fbdc2983bbda10753130c532d73bc310
chankaran2996 <chandrasekaran2996@gmail.com>
2023-03-15 00:47:26 +0530

    day2

--- src/components/user.js
+++ src/components/user.js
@@ -1,10 +1,12 @@
-import autoprefixer from "autoprefixer";
 import React from "react";
+// importing link form react-router-dom
+import {Link} from "react-router-dom"
 
+// creating user componet 
 const User = () => {
     return(
         <div className="container mx- auto">
-            <div className="flex justify-center items-center h-screen">
+            <div className="flex h-screen">
                 <div className="title flex flex-col items-center">
                     <h4 className="text-5xl font-bold">hello</h4>
                     <span className="py-4 text-xl w-2/3 text-center text-gray-500">
@@ -13,6 +15,13 @@ const User = () => {
                     <form className="py-1">
                         <div className="profile flex justify-center py-5">
                             <img src="" alt="profile-icon"></img>
+                        </div>
+                        <div className="textbox flex flex-col items-center gap-6">
+                            <input type="text" placeholder="username"/>
+                            <button type="submit">Lets Go</button>
+                        </div>
+                        <div className="text-center py-4">
+                            <span className="text-gray-500">Not a member?<Link className="text-red-500" to="/register">Register now</Link></span>
                         </div>
                     </form>
                 </div>
