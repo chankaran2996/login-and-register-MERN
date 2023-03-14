c42cd55b3e64fe7b083b5ca013c5396ee9702338
chankaran2996 <chandrasekaran2996@gmail.com>
2023-03-14 21:34:59 +0530

    day1.3

--- src/components/user.js
+++ src/components/user.js
@@ -5,8 +5,16 @@ const User = () => {
     return(
         <div className="container mx- auto">
             <div className="flex justify-center items-center h-screen">
-                <div className="title">
-                    <h1>hello</h1>
+                <div className="title flex flex-col items-center">
+                    <h4 className="text-5xl font-bold">hello</h4>
+                    <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+                        Explore More by connecting with us.
+                    </span>
+                    <form className="py-1">
+                        <div className="profile flex justify-center py-5">
+                            <img src="" alt="profile-icon"></img>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
