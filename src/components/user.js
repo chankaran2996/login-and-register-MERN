adf04b11fd6c7db1e88f5ffc190b9d43b8d96942
chankaran2996 <chandrasekaran2996@gmail.com>
2023-03-15 23:23:32 +0530

    day2.2 login UI

--- src/components/user.js
+++ src/components/user.js
@@ -4,31 +4,33 @@ import {Link} from "react-router-dom";
 // importing profile image
 import profile from '../assets/profile.png';
 // improting css for user component 
-import styles from "../styles/user.css";
+import styles from "../styles/User.module.css";
 
 // creating user componet 
 const User = () => {
     return(
         <div className="container mx- auto">
             <div className="flex iterms-center justify-center h-screen">
+              <div className={styles.glass}>  
                 <div className="title flex flex-col items-center">
-                    <h4 className="text-5xl font-bold">hello</h4>
+                    <h4 className="text-5xl font-bold">Hello!</h4>
                     <span className="py-4 text-xl w-2/3 text-center text-gray-500">
                         Explore More by connecting with us.
                     </span>
                     <form className="py-1">
                         <div className="profile flex justify-center py-5">
-                            <img src={profile} alt="profile-icon"></img>
+                            <img src={profile} className={styles.profile_img} alt="profile-icon"></img>
                         </div>
                         <div className="textbox flex flex-col items-center gap-6">
-                            <input type="text" placeholder="username"/>
-                            <button type="submit">Lets Go</button>
+                            <input type="text" className={styles.textbox} placeholder="username"/>
+                            <button type="submit" className={styles.btn}>Lets Go</button>
                         </div>
                         <div className="text-center py-4">
                             <span className="text-gray-500">Not a member?<Link className="text-red-500" to="/register">Register now</Link></span>
                         </div>
                     </form>
                 </div>
+              </div>  
             </div>
         </div>
     )
