b83281b03eaab3eb3683b1c394469c3b85b27911
swathySuriyababu12 <swathysuryababu@gmail.com>
2023-03-30 23:16:42 +0530

    register

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -1,98 +1,104 @@
-// improting Toster which use to give some toser like popup or noticfication 
+// improting Toster which use to give some toser like popup or noticfication
 import { toast } from "react-hot-toast";
 
-// validate login page username and exporting 
+// validate login page username and exporting
 export const userNameValidate = async (value) => {
-    const error = userNameVerify({},value);
+  const error = userNameVerify({}, value);
 
-    return error;
-}
+  return error;
+};
 
-//validate profile page 
-export const  profileValidation = async (value) => {
-    const errors = emailVerify({}, value);
-    return errors;
-}
+//validate profile page
+export const profileValidation = async (value) => {
+  const errors = emailVerify({}, value);
+  return errors;
+};
 
 // validate register form
 export const registerValidation = async (value) => {
-    const errors = userNameVerify({}, value);
-    conformPasswordVerify(errors, value);
-    emailVerify(errors, value);
+  const errors = userNameVerify({}, value);
+  conformPasswordVerify(errors, value);
+  emailVerify(errors, value);
 
-    return errors;
-}
-// validate  password and exporting 
+  return errors;
+};
+// validate  password and exporting
 export const passwordValidate = async (value) => {
-    const error = passwordVerify({},value);
+  const error = passwordVerify({}, value);
 
-    return error;
-}
+  return error;
+};
 
-// validate  canform password and exporting 
+// validate  canform password and exporting
 export const conformPasswordValidate = async (value) => {
-    const error = conformPasswordVerify({},value);
+  const error = conformPasswordVerify({}, value);
 
-    return error;
-}
-
-// Validate password
-const conformPasswordVerify = (error={},value) => {
-
-    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-    if(value.password!==value.conform_password){
-        error.password = toast.error('Password and comform password must be same...!');
-    }else if(!value.password){
-        error.password = toast.error('Password Required...!');
-    }else if(value.password.includes(" ")){
-        error.username = toast.error('Password should not have space ...!');
-    }else if(value.password.length<5){
-        error.username = toast.error('Password must be more than 5 character...!')
-    }else if(!specialChars.test(value.password)){
-        error.username = toast.error('Password must have specialChars...!')
-    }
-    return error;
-
-}
+  return error;
+};
 
 // Validate password
-const passwordVerify = (error={},value) => {
+const conformPasswordVerify = (error = {}, value) => {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  if (value.password !== value.conform_password) {
+    error.password = toast.error(
+      "Password and comform password must be same...!"
+    );
+  } else if (!value.password) {
+    error.password = toast.error("Password Required...!");
+  } else if (value.password.includes(" ")) {
+    error.username = toast.error("Password should not have space ...!");
+  } else if (value.password.length < 5) {
+    error.username = toast.error("Password must be more than 5 character...!");
+  } else if (!specialChars.test(value.password)) {
+    error.username = toast.error("Password must have specialChars...!");
+  }
+  return error;
+};
 
-    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+// Validate password
+const passwordVerify = (error = {}, value) => {
+  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
-    if(!value.password){
-        error.password = toast.error('Password Required...!');
-    }else if(value.password.includes(" ")){
-        error.username = toast.error('Wrong password...!');
-    }else if(value.password.length<5){
-        error.username = toast.error('Password must be more than 5 character...!')
-    }else if(!specialChars.test(value.password)){
-        error.username = toast.error('Password must have specialChars...!')
-    }
-    return error;
-
-}
+  if (!value.password) {
+    error.password = toast.error("Password Required...!");
+  } else if (value.password.includes(" ")) {
+    error.username = toast.error("Wrong password...!");
+  } else if (value.password.length < 5) {
+    error.username = toast.error("Password must be more than 5 character...!");
+  } else if (!specialChars.test(value.password)) {
+    error.username = toast.error("Password must have specialChars...!");
+  }
+  return error;
+};
 
 // Validate username
-const userNameVerify = (error={},value) => {
-    if(!value.username){
-        error.username = toast.error('Username Required...!');
-    }else if(value.username.includes(" ")){
-        error.username = toast.error('Invalid Username...!')
-    }
-    return error;
+// const userNameVerify = (error={},value) => {
+//     if(!value.username){
+//         error.username = toast.error('Username Required...!');
+//     }else if(value.username.includes(" ")){
+//         error.username = toast.error('Invalid Username...!')
+//     }
+//     return error;
 
-}
+// }
+const userNameVerify = (error = {}, value) => {
+  if (!value.name) {
+    error.name = toast.error("Username Required...!");
+  } else if (value.name.includes(" ")) {
+    error.name = toast.error("Invalid Username...!");
+  }
+  return error;
+};
 
 // validating email
-const  emailVerify = (error ={}, value) => {
-    if(!value.email){
-        error.email = toast.error("Email Required...!");
-    }else if(value.email.includes(" ")){
-        error.email = toast.error("Wrong Email...!")
-    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)){
-        error.email = toast.error("Invalid email address...!")
-    }
+const emailVerify = (error = {}, value) => {
+  if (!value.email) {
+    error.email = toast.error("Email Required...!");
+  } else if (value.email.includes(" ")) {
+    error.email = toast.error("Wrong Email...!");
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)) {
+    error.email = toast.error("Invalid email address...!");
+  }
 
-    return error;
-}
+  return error;
+};
